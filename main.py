901dad75f476d3d047baf2e3ee5ce417b5ddca20
Alexander Baransky <alexander.baranskiy@yandex.ru>
2022-01-03 17:57:22 +0300

    Add CORS middleware Fixes CORS requests in browsers (enabled by default)

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from enum import Enum
 import platform
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS configuration. Edit if necessary
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/os")
