22d6998e5b9a27c34017da3845f60f0c200ab5d1
Alexander Baransky <alexander.baranskiy@yandex.ru>
2022-01-05 14:38:02 +0300

    Edit memory module to use "human" GET parameter Ability to select human-readable output

--- modules/memory.py
+++ modules/memory.py
@@ -1,45 +1,54 @@
 from main import app
+from fastapi import Query
+
 import psutil
 from psutil._common import bytes2human
 
 from pydantic import BaseModel
+from typing import Optional, Union
 
 
-class MemoryCommonModel(BaseModel):
+class RAMModel(BaseModel):
     percent: float
-    raw: dict
-    human: dict
+    total: Union[int, str]
+    available: Union[int, str]
+    used: Union[int, str]
+    free: Union[int, str]
+    active: Optional[Union[int, str]] = None
+    inactive: Optional[Union[int, str]] = None
+    buffers: Optional[Union[int, str]] = None
+    cached: Optional[Union[int, str]] = None
+    shared: Optional[Union[int, str]] = None
+    slab: Optional[Union[int, str]] = None
+    wired: Optional[Union[int, str]] = None
+
+
+class SwapModel(BaseModel):
+    percent: float
+    total: Union[int, str]
+    used: Union[int, str]
+    free: Union[int, str]
+    sin: Union[int, str]
+    sout: Union[int, str]
 
 
 class MemoryResponseModel(BaseModel):
-    ram: MemoryCommonModel
-    swap: MemoryCommonModel
+    ram: RAMModel
+    swap: SwapModel
 
 
 @app.get('/memory', response_model=MemoryResponseModel)
-async def get_memory_info():
+async def get_memory_info(human: Optional[bool] = Query(
+        False, title='Human-readable', description='If set to true, values are sent as strings with unit postfix')):
     memory = psutil.virtual_memory()
     swap = psutil.swap_memory()
     payload = {
-        'ram': {
-            'percent': memory.percent,
-            'raw': {},
-            'human': {}
-        },
-        'swap': {
-            'percent': swap.percent,
-            'raw': {},
-            'human': {}
-        }
+        'ram': {},
+        'swap': {}
     }
     for key in payload.keys():
-        for key2 in payload[key].keys():
-            if key2 == 'percent':
-                continue
-            obj = memory if key == 'ram' else swap
-            for key3 in obj._fields:
-                if key3 == 'percent':
-                    continue
-                value = getattr(obj, key3)
-                payload[key][key2][key3] = value if key2 == 'raw' else bytes2human(value)
+        obj = memory if key == 'ram' else swap
+        for key2 in obj._fields:
+            value = getattr(obj, key2)
+            payload[key][key2] = bytes2human(value) if key2 != 'percent' and human else value
     return payload
